72986c796efb2475e303c4b31b04fb404faf5e5f
Jorge Gonzalez Regalado <61213914+jregaladoo@users.noreply.github.com>
2026-02-13 17:26:16 +0200

    Remove favicon

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -32,7 +32,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Luckiest+Guy&family=Plus+Jakarta+Sans:wght@400;700;800&display=swap" rel="stylesheet" />
